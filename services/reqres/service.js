e50601a4f26a7f05fedb21f9e24b0cd26754ac1e
Macky Bugarin <manuel.bugarin@paymaya.com>
2022-03-15 16:37:32 +0800

    Added getSingleUser for Reqres service

--- services/reqres/service.js
+++ services/reqres/service.js
@@ -1,21 +1,30 @@
-const packages = require('../../utils/packages');
+const packages = require("../../utils/packages");
 
-class Reqres { 
-    constructor(baseUrl, package) { 
-        this.baseUrl = baseUrl;
-        this.sendRequest = packages[package].sendRequest;
-        this.payload = {}
-    }
+class Reqres {
+  constructor(baseUrl, package) {
+    this.baseUrl = baseUrl;
+    this.sendRequest = packages[package].sendRequest;
+    this.payload = {};
+  }
 
-    getUsersList(params) {
-        const config = {
-            baseUrl = this.baseUrl,
-            params: params,
-            path: '/api/users'
-        }
-        const response = this.sendRequest(config);
-        return response;
-    }
+  getUsersList(params) {
+    const config = {
+      baseUrl: this.baseUrl,
+      params: params,
+      path: "/api/users",
+    };
+    const response = this.sendRequest(config);
+    return response;
+  }
+
+  getSingleUser(id) {
+    const config = {
+      base: this.baseUrl,
+      path: "/api/users/" + id,
+    };
+    const response = this.sendRequest(config);
+    return response;
+  }
 }
 
-module.exports = Reqres
+module.exports = Reqres;
